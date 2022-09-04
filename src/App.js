b64f534d21b4535872229eb581e22c5be1d8797c
shailu <ssrapol06@gmail.com>
2022-09-04 23:41:24 +0530

    instagram-clone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div  className='bg-purple-600  px-[660px]  font-bold text-2xl m-4 rounded-3xl  py-20'>Sh@!luuuuuuuuuuuuu</div>
+  )
 }
 
-export default App;
+export default App
